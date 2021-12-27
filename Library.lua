90c9dea9cfcc880b6fd8f9af0c8761639ef86296
wally <38594850+wally-rblx@users.noreply.github.com>
2021-12-27 04:11:09 -0600

    Fix an error?
    Attempt to fix an error where `KeyPicker.Mode` is set to nothing and causes `KeyPicker:Update` to error

--- Library.lua
+++ Library.lua
@@ -597,8 +597,6 @@ do
             end;
 
             function ModeButton:Deselect()
-                KeyPicker.Mode = nil;
-
                 Label.TextColor3 = Library.FontColor;
                 Library.RegistryMap[Label].Properties.TextColor3 = 'FontColor';
             end;
