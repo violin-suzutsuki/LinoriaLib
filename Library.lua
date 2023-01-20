14cf7303c118d3a5a275b600452805781387e1ee
wally <38594850+wally-rblx@users.noreply.github.com>
2023-01-20 16:08:03 -0600

    Fix typos

--- Library.lua
+++ Library.lua
@@ -33,7 +33,7 @@ local Library = {
     BackgroundColor = Color3.fromRGB(20, 20, 20);
     AccentColor = Color3.fromRGB(0, 85, 255);
     OutlineColor = Color3.fromRGB(50, 50, 50);
-    RiskColor = Color3.fromRGB(255, 50, 50)
+    RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
     Font = Enum.Font.Code,
