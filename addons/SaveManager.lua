1284086c700dbc7e23bed49f5c0f7b02f1f78654
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-04-29 19:12:35 +0200

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -124,7 +124,7 @@ local SaveManager = {} do
 
 		for _, option in next, decoded.objects do
 			if self.Parser[option.type] then
-				self.Parser[option.type].Load(option.idx, option)
+				task.spawn(function() self.Parser[option.type].Load(option.idx, option) end) -- task.spawn() so the config loading wont get stuck.
 			end
 		end
 
