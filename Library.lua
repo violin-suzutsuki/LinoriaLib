26fbb7f10c5b9c122b409fa9a28a781578e178bd
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-05-23 15:18:02 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -51,7 +51,7 @@ local Library = {
     IsMobile = false;
     DevicePlatform = Enum.Platform.None;
     CanDrag = true;
-	ShowCustomCursor = true;
+	ShowCustomCursor = true; 
 };
 
 pcall(function() Library.DevicePlatform = InputService:GetPlatform(); end); -- For safety so the UI library doesn't error.
