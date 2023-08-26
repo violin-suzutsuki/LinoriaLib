86556518c7f818a2d5c2372f3857e1fb68b04c43
Incognito <75094867+lncoognito@users.noreply.github.com>
2023-08-26 23:46:34 +0300

    Update WTF.lua

--- WTF.lua
+++ WTF.lua
@@ -1,11 +1,11 @@
 -- Ignore this is for fondra.
 
-Fondra.DownloadAsset("WTF.mp3", "https://github.com/lncoognito/Assets/raw/main/Sounds/WTF.wav")
+Fondra.DownloadAsset("Sounds/WTF.mp3", "https://github.com/lncoognito/Assets/raw/main/Sounds/WTF.wav")
 
 local Sound 		= Instance.new("Sound")
 Sound.Parent 		= workspace
 Sound.Volume		= 2.5
-Sound.SoundId   	= Fondra.GetAsset("WTF.mp3")
+Sound.SoundId   	= Fondra.GetAsset("Sounds/WTF.mp3")
 Sound:Play()
 
 Fondra.Services.Debris:AddItem(Sound, 2)
